f3f4b1ae2653f0a58b6b7da2eb1cbc73468d9936
Mugisha Elie <iradukunda2k24@gmail.com>
2025-06-14 10:29:15 +0200

    prime checker

--- Day3_Functions.js
+++ Day3_Functions.js
@@ -1,9 +1,14 @@
-function reverseString(reverseMe){
-    var reversed = "";
-    for (var i = reverseMe.length-1; i>=0; i--){
-        reversed += reverseMe[i];
+function isPrime(integer){
+    for(let x=2; x<integer; x++ ){
+        if(integer % x === 0){
+            console.log(integer+ " is divisible by "+x);
+            return false;
+        }
+        
     }
-    return reversed;
+    
+    return true
+
 }
 
-console.log(reverseString("Amata"));
+isPrime(24);
